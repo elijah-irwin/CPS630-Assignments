d0937a8c75191b13aaa9d09a01605283bc90df6f
McKenzie Irwin <mak_irwin@hotmail.com>
2018-01-31 23:51:56 -0500

    Initial files, added hide/show fadein/out functionality with radio buttons

---
+++ Labs/Lab02/app.js
@@ -0,0 +1,23 @@
+function show() {
+	$("#img1").fadeIn();
+}
+
+function hide() {
+	$("#img1").fadeOut();
+}
+
+function show2() {
+	$("#img2").fadeIn();
+}
+
+function hide2() {
+	$("#img2").fadeOut();
+}
+
+function show3() {
+	$("#img3").fadeIn();
+}
+
+function hide3() {
+	$("#img3").fadeOut();
+}
