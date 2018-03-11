9194454a93622ef166b170cceb97bcaefa825300
McKenzie Irwin <mak_irwin@hotmail.com>
2018-03-11 14:37:48 -0400

    clean up presentation of home page

--- Assignment2/scripts/A2Q1.js
+++ Assignment2/scripts/A2Q1.js
@@ -1,53 +1,92 @@
 var sn = new Object();
-sn.photo = "../assets/starryNight.jpg"
+sn.photo = "assets/starryNight.jpg";
 sn.name = "The Starry Night";
 sn.artist = "Vincent Van Gogh";
 sn.dofp = "1889";
 sn.dimensions  = "73.7cm x 92.1cm";
 sn.curlocation = "Museum of Modern Art";
-sn.medium = "Oil On Canvas"
+sn.medium = "Oil On Canvas";
 sn.genre = "Post-Impressionism, Modern";
+sn.price = "$15,000";
 sn.description = "Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an idealized village.";
-sn.price = "$15,000"
+
+var vvg = new Object();
+vvg.photo = "assets/vvg.jpg";
+vvg.name = "Vincent Van Gogh";
+vvg.dofb = "<br>March 30, 1853 - July 29, 1890";
+vvg.location = "Netherlands";
+vvg.genre = "<br>Post-Impressionism";
+vvg.famousWorks = "<br>The Starry Night,<br>Sorrow,<br>Sunflowers";
+vvg.description = "Vincent Willem van Gogh was born on March 30, 1853 in Zundert, Netherlands. His art highly influenced artwork of the 20th-century. His most famous works include 'Starry Night', 'Irises', and 'Sunflowers'. He died in France on July 29, 1890 at the age of 37." ;
+
 
 var adam = new Object();
-adam.photo = "../assets/adam.jpg";
+adam.photo = "assets/adam.jpg";
 adam.name = "The Creation of Adam";
 adam.artist = "Michelangelo";
 adam.dofp = "1508-1512";
 adam.dimensions  = "2.8m x 5.7m";
 adam.curlocation = "Sistine Chapel";
-adam.medium = "Paint, Plaster"
+adam.medium = "Paint, Plaster";
 adam.genre = "Renaissance";
 adam.description = "It illustrates the Biblical creation narrative from the Book of Genesis in which God gives life to Adam, the first man. ";
-adam.price = "$20,000"
+adam.price = "$20,000";
+
+var m = new Object();
+m.photo = "assets/m3.jpg";
+m.name = "Michelangelo";
+m.dofb = "<br>March 6, 1475";
+m.location = "Italy";
+m.genre = "<br>High Renaissance";
+m.famousWorks = "<br>The Creation of Adam,<br>David,<br>The Last Judgement";
+m.description = "Michelangelo was born March 6, 1475 in Caprese, Italy. He was considered one of the most famous artists of the Italian Renaissance. Michelangelo was many things: sculptor, poet, architect and painter. He died in Rome, Italy on February 18, 1564 at the age of 88.";
 
 var kiss = new Object();
-kiss.photo = "../assets/kiss.jpg";
+kiss.photo = "assets/kiss.jpg";
 kiss.name = "The Kiss";
 kiss.artist = "Gustav Klimt";
 kiss.dofp = "1907-1908";
 kiss.dimensions  = "1.8m x 1.8m";
 kiss.curlocation = "Osterreichische <br> Galerie Belvedere";
-kiss.medium = "Oil On Canvas"
+kiss.medium = "Oil On Canvas";
 kiss.genre = "Symbolism, Modern";
 kiss.description = "The painting depicts a couple embracing one another, their bodies entwined in elaborate robes decorated in a style influenced by the contemporary Art Nouveau style and the organic forms of the earlier Arts and Crafts movement. ";
-kiss.price = "$18,000"
+kiss.price = "$18,000";
+
+var klimt = new Object();
+klimt.photo = "assets/klimt.jpg";
+klimt.name = "Gustav Klimt";
+klimt.dofb = "<br>July 14, 1862 - Feb. 6, 1918";
+klimt.location = "Austria";
+klimt.genre = "<br>Symbolism, Art Nouveau";
+klimt.famousWorks = "<br>The Kiss,<br>Danae,<br>Portrait of Adele Bloch-Bauer I";
+klimt.description = "Gustav Klimt was born on July 14, 1862 in Vienna, Austria. Klimt became known due to his highly decorative style and for the errotic nature of his work. He died due to pneumonia on February 6, 1918 in Austria." ;
+
 
 var ml = new Object();
-ml.photo = "../assets/monalisaResize.jpg";
+ml.photo = "assets/monalisaResize.jpg";
 ml.name = "Mona Lisa";
 ml.artist = "Leonardo da Vinci";
 ml.dofp = "1503-(1506-1517)";
 ml.dimensions  = "77cm x 53cm";
 ml.curlocation = "Musee du Louvre, Paris";
-ml.medium = "Oil On Panel"
+ml.medium = "Oil On Panel";
 ml.genre = "Portrait";
 ml.description = "The painting is thought to be a portrait of Lisa Gherardini, the wife of Francesco del Giocondo.";
-ml.price = "$30,000"
+ml.price = "$30,000";
+
+var ldv = new Object();
+ldv.photo = "assets/ldv.jpg"
+ldv.name = "Leonardo Da Vinci";
+ldv.dofb = "<br>April 15, 1452 - May 2, 1519";
+ldv.location = "Italy";
+ldv.genre = "High Renaissance";
+ldv.famousWorks = "<br>Mona Lisa,<br>The Last Supper,<br>The Vitruvian Man";
+ldv.description = "Leondardo da Vinci was born on April 15, 1452 in Vinci, Italy. Da Vinci was many things: painter, sculptor, architect, inventor, military engineer and mathmetician. His most known works are 'The Last Supper' and the 'Mona Lisa'. He died on May 2, 1519 in Amboise, France.";
+
 
 var s = new Object();
-s.photo = "../assets/screamResize.jpg";
+s.photo = "assets/screamResize.jpg";
 s.name = "The Scream";
 s.artist = "Edvard Munch";
 s.dofp = "1893";
@@ -56,10 +95,20 @@ s.curlocation = "National Gallery, Norway";
 s.medium = "Oil, Tempera, Pastel, <br> Crayon on Cardboard";
 s.genre = "Expressionism";
 s.description = "The works show a figure with an agonized expression against a landscape with a tumultuous orange sky.";
-s.price = "$45,000"
+s.price = "$45,000";
+
+var em = new Object();
+em.photo = "assets/em.jpg"
+em.name = "Edvard Munch";
+em.dofb = "<br>Dec. 12, 1863 - Jan. 23, 1944";
+em.location = "Norway";
+em.genre = "<br>Expressionism, Symbolism";
+em.famousWorks = "<br>The Scream,<br>Madonna,<br>The Sick Child";
+em.description = "Edvard Munch was born on December 12, 1863 in Loten, Norway. His most well known painting is 'The Scream' which sold for more tha $119 million in New York setting an all new record. He died on January 23, 1944 in Ekely Norway.";
+
 
 var pearl = new Object();
-pearl.photo = "../assets/final.jpg";
+pearl.photo = "assets/final.jpg";
 pearl.name = "Girl with a Pearl Earring";
 pearl.artist = "Johannes Vermeer";
 pearl.dofp = "1665";
@@ -68,10 +117,20 @@ pearl.curlocation = "Mauritshuis, Netherlands";
 pearl.medium = "Oil On Canvas";
 pearl.genre = "Portrait";
 pearl.description = "It is a tronie of a girl wearing a headscarf and a pearl earring. ";
-pearl.price = "$21,000"
+pearl.price = "$21,000";
+
+var jv = new Object();
+jv.photo = "assets/jv.jpg";
+jv.name = "Johannes Vermeer";
+jv.dofb = "<br>Oct. 31, 1632 - Dec. 15, 1675";
+jv.location = "Netherlands";
+jv.genre = "<br>Dutch Golden Age, Baroque";
+jv.famousWorks = "<br>The Procuress,<br>The Little Street,<br>View of Delft";
+jv.description = "Johannes Vermeer was born on October 31, 1632 in Delft, Netherlands. He is one of the most recongnized Dutch artists of all time. Vermeer's most popular paintings include 'Little Street', 'View of Delft' and 'Girl with a Pearl Earring'.  He died December 16, 1675 in the Netherlands.";
+
 
 var array = [sn,adam,kiss,ml,s,pearl];
-
+var artist = [vvg,m,klimt,ldv,em,jv];
 
 // When the user clicks the button,
 function displayInfo(value) {
@@ -81,9 +140,25 @@ function displayInfo(value) {
 	var price		= array[value].price;
 
 	document.getElementById("photo").src = img;
-	document.getElementById("name").innerHTML = "<strong>Name: </strong>" + name;
+	document.getElementById("name").innerHTML = "<strong>Name: </strong>"  + "<a href=./Pages/A2Q3.html onclick=check()>" + name;
 	document.getElementById("description").innerHTML = "<strong>Description: </strong>" + description;
 	document.getElementById("price").innerHTML = "<strong>Price: </strong>" + price;
 	
 }
+
+function displayArtist(value) {
+	var img         = artist[value].photo;
+	var name        = artist[value].name;
+	var description = artist[value].description;
+	
+	document.getElementById("photo").src = img;
+	document.getElementById("name").innerHTML = "<h2>"   + name + "</h2>";
+	document.getElementById("description").innerHTML = "<strong>Description: </strong>" + description;
+	document.getElementById("price").innerHTML = " ";
+
+}
+
+
+
+
 
